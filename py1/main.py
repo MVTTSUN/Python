0d0ff17ad8fbf922a91a4cf61843d1633f1ea727
MVTT <ryabtzun2011@yandex.ru>
2022-12-14 21:36:39 +0300

    add python 1

--- py1/main.py
+++ py1/main.py
@@ -1,38 +1,50 @@
 import math
 
-#1
-def day_week(number):
-  if type(number) == int:
-    number = str(number)
-  match number:
-    case '1':
-      print('Понедельник')
-    case '2':
-      print('Вторник')
-    case '3':
-      print('Среда')
-    case '4':
-      print('Четверг')
-    case '5':
-      print('Пятница')
-    case '6':
-      print('Суббота')
-    case '7':
-      print('Воскресенье')
-    case _:
-      print('Нет такого дня')
+# 1
+def is_holiday(number):
+  print(f'{number} - это выходной день?')
+  if number >= 6 and number <= 7:
+    print('да')
+  elif number >= 1 and number <= 5:
+    print('нет')
+  else:
+    print('введите число от 1 до 7')
 
-day_week(3)
+is_holiday(7)
 
-#2
-def distance_in_2d(a, b):
-  res = math.sqrt(pow(b[0] - a[0], 2) + pow(b[1] - a[1], 2))
-  print(res)
+# 2
+def is_truth(x, y, z):
+  print(f'x = {x}, y = {y}, z = {z}')
+  if not (x or y or z) == (not x) and (not y) and (not z):
+    print('Утверждение истинно')
+  else:
+    print('Утверждение ложно')
 
-distance_in_2d([7, -5], [1, -1])
 
-#3
-def term(number):
+is_truth(0, 0, 0)
+
+# 3
+def get_term(x, y):
+  print(f'Координаты x = {x}, y = {y}. Где на координатной плоскости расположена точка?')
+  if x > 0 and y > 0:
+    print('1 четверть')
+  elif x < 0 and y > 0:
+    print('2 четверть')
+  elif x < 0 and y < 0:
+    print('3 четверть')
+  elif x > 0 and y < 0:
+    print('4 четверть')
+  elif x == 0 and y != 0:
+    print('лежит на оси x')
+  elif x != 0 and y == 0:
+    print('лежит на оси y')
+  else:
+    print('лежит на пересечении оси x и y')
+
+get_term(34, -30)
+
+# 4
+def get_coordinates(number):
   if type(number) == int:
     number = str(number)
   match number:
@@ -46,13 +58,12 @@ def term(number):
       print('x > 0, y < 0')
     case _:
       print('нет такой четверти')
-      
-term(4)
 
-#4
-def odd_numbers(n):
-  for i in range(1, n + 1):
-    if i % 2 == 0:
-      print(i, end = ' ')
+get_coordinates(3)
 
-odd_numbers(7)
+# 5
+def find_2d_distance(a, b):
+  print(f'Точка А ({a[0], a[1]}), точка B ({b[0], b[1]}). Расстояние между точками А и В:')
+  print(math.sqrt(pow(b[0] - a[0], 2) + pow(b[1] - a[1], 2)))
+
+find_2d_distance([7, -5], [1, -1])
